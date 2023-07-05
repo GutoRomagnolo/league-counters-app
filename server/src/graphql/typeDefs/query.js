2e889fd9d67f6ff2f5da66909a961eada88979c0
Gustavo Ramos Romagnolo <gustavo.romagnolo@eldorado.org.br>
2023-07-05 00:31:21 -0300

    refac: removing graphql and starting to use standard express

--- server/src/graphql/typeDefs/query.js
+++
@@ -1,8 +0,0 @@
-const { gql } = require("apollo-server");
-const query = gql`
-  type Query {
-    champions: [Champion]
-    champion(id: ID!): Champion
-  }
-`;
-module.exports = query;
---
+++ server/src/routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router()
+
+router.post('/champion', (request, response) => {
+});
+
+export { router }
