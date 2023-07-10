e5a32697fc484a6aec24715b9f715b0c16a71e8f
Gustavo Ramos Romagnolo <gustavo.romagnolo@eldorado.org.br>
2023-07-09 22:58:26 -0300

    fix(server: champion-prisma-repository): removing console.log

--- server/src/infra/database/prisma/champion-prisma-repository.ts
+++ server/src/infra/database/prisma/champion-prisma-repository.ts
@@ -3,7 +3,6 @@ import { CreateChampionRepository } from '@/data/protocols/database'
 
 export class ChampionPrismaRepository implements CreateChampionRepository {
   async create(championData: CreateChampionRepository.Params): Promise<CreateChampionRepository.Result> {
-    console.log("data", championData)
     const championResponse = await PrismaHelper.client.champion.create({ data: championData })
     return championResponse !== null
   }
