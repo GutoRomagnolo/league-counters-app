9c57640ef166351dbe9159665fc161d3f6e8b8ac
Gustavo Ramos Romagnolo <gustavo.romagnolo@eldorado.org.br>
2023-07-09 22:56:18 -0300

    feat(server: env): adding external api url into env file

--- server/src/main/config/env.ts
+++ server/src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
   port: 5050,
-  postgresUrl: 'postgresql://postgres:league123@localhost:5432/league-toplane-counters?schema=public'
+  postgresUrl: 'postgresql://postgres:league123@localhost:5432/league-toplane-counters?schema=public',
+  dragonApiBaseUrl: 'https://raw.communitydragon.org'
 }
 
